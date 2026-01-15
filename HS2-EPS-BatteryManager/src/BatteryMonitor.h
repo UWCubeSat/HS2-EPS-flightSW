985b294081ddeba8486ebd6a26f6a15ad67e52ad
Tanmay <tanmay.x@hotmail.com>
2026-01-14 22:57:40 -0800

    modified class structure

--- HS2-EPS-BatteryManager/src/BatteryMonitor.h
+++ HS2-EPS-BatteryManager/src/BatteryMonitor.h
@@ -1,10 +1,16 @@
 #include "BQ25756_reg.h"
 #include "i2c.h"
 
-int getVac();
-int getVbat();
-int getVfb();
-int getIac();
-int getIbat();
-int getIchg();
-chargingStatus getChargingStatus();
+class BatteryMonitor : public BQ25756{
+    public:
+        void report_status();
+        void printVBAT_LOWV();
+    private:
+        int getVac();
+        int getVbat();
+        int getVfb();
+        int getIac();
+        int getIbat();
+        int getIchg();
+        chargingStatus getChargingStatus();
+};
