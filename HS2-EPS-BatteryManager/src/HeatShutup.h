a2a68d7de2087995e5a835e65bb3352b9a007896
PearSimionRaprace <benqian004@gmail.com>
2026-01-23 16:56:48 -0800

    save work

--- HS2-EPS-BatteryManager/src/HeatShutup.h
+++ HS2-EPS-BatteryManager/src/HeatShutup.h
@@ -4,8 +4,45 @@
 #include "BQ25756_reg.h"
 #include "i2c.h"
 
-class HeatShutup: public BQ25756{
+class HeatShutup{
     public:
+        enum TS_LVL {
+            TS_NORMAL,
+            TS_WARM,
+            TS_COOL,
+            TS_COLD,
+            TS_HOT,
+            TS_INVALID
+        };
+
+        enum TS_T5_prcnt { 
+            T5_41p2,
+            T5_37p7,
+            T5_34p375,
+            T5_31p25
+        };
+
+        enum TS_T3_prcnt {
+            T3_48p4,   
+            T3_44p8,   
+            T3_41p2,   
+            T3_37p7 
+        };
+
+        enum TS_T2_prcnt { 
+            T2_71p1,  
+            T2_68p4,   
+            T2_65p5,   
+            T2_62p4 
+        };
+        
+        enum TS_T1_prcnt { 
+            T1_77p15,  
+            T1_75p32,  
+            T1_73p25,  
+            T1_71p1 
+        };
+
         TS_LVL readTS_STAT();
         float TS_ADC();
         void JEITA_enable();
