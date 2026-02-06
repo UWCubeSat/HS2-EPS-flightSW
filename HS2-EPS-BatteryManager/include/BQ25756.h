50cedf790cb666c15c75f43f61e56d6ea7a163ca
Momoka Sakamoto <momoka17@uw.edu>
2026-02-05 22:39:54 -0800

    Replace BQ25756.cpp and BQ25756.h with better version

--- HS2-EPS-BatteryManager/include/BQ25756.h
+++ HS2-EPS-BatteryManager/include/BQ25756.h
@@ -1,18 +1,27 @@
 #ifndef BQ25756_H
 #define BQ25756_H
 
+#include <Arduino.h>
 #include "i2c.h"
 #include "BQ25756_reg.h"
 
 
 class BQ25756 {
     public:
-        void printInitializationStatus();
         class ADCControl {
             public:
+                // bool functions
+                bool isADCEnabled();
+                bool isADCRateOneshot();
+                bool isIBAT_ADCDisabled();
+                bool isIAC_ADCDisabled();
+                bool isVAC_ADCDisabled();
+                bool isVBAT_ADCDisabled(); 
+                bool isTS_ADCDisabled();
+                bool isVFB_ADCDisabled();
+                
+                // Enable ADC functions
                 void enableADC();
-                bool ADCEnabled();
-                bool getADCRate();
                 void enableIAC_ADC();
                 void enableIBAT_ADC();
                 void enableVAC_ADC();
@@ -20,6 +29,15 @@ class BQ25756 {
                 void enableTS_ADC();
                 void enableVFB_ADC();
                 void enableAllADCControl();
+
+                // Disable ADC functions
+                void disableADC();
+                void disableIAC_ADC();
+                void disableIBAT_ADC();
+                void disableVAC_ADC();
+                void disableVBAT_ADC();
+                void disableTS_ADC();
+                void disableVFB_ADC();
         };
     ADCControl adc;    
 };
