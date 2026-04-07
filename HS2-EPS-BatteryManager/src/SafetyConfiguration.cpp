1fb6a83a60a3fefeb1f4ea674241c63217a21757
Momoka Sakamoto <momoka17@uw.edu>
2026-04-07 11:15:08 -0700

    Replace safetyconfig

--- HS2-EPS-BatteryManager/src/SafetyConfiguration.cpp
+++ HS2-EPS-BatteryManager/src/SafetyConfiguration.cpp
@@ -18,6 +18,23 @@ bool BQ25756::SafetyConfig::WatchdogTimerControlDisabled()
     return ((read8bitRegister(TIME_CONT) & 0x30) == 0);
 }
 
+
+// Disable MPPT
+void BQ25756::SafetyConfig::disableMPPT()
+{
+    uint8_t currValue = read8bitRegister(MPPT_CONT);
+    uint8_t newValue = currValue & ~(0x01);
+    writeRegister(MPPT_CONT, newValue);
+}
+
+// Enable MPPT
+void BQ25756::SafetyConfig::enableMPPT()
+{
+    uint8_t currValue = read8bitRegister(MPPT_CONT);
+    uint8_t newValue = currValue | 0x01;
+    writeRegister(MPPT_CONT, newValue);
+}
+
 /** 
  * @brief Return if MPPT is enabled
  * 
@@ -30,6 +47,7 @@ bool BQ25756::SafetyConfig::MPPTenabled()
 }
 
 // Enable TS pin function control
+// This applies to forward charging and reverse discharging modes
 void BQ25756::SafetyConfig::EnableTSPinFunctionControl()
 {
     uint8_t currValue = read8bitRegister(CHARGE_REGION_CONT);
@@ -75,6 +93,14 @@ bool BQ25756::SafetyConfig::PGFlagNormal()
     return (((currValue >> 7) & 0x01) == 0);
 }
 
+// Check Charge enable status: EN_CHG
+// Return
+//      bool: True if enabled, otherwise false
+bool BQ25756::SafetyConfig::chargeEnableControl()
+{
+    return (read8bitRegister(CHARGER_CONT) & (0x01));
+}
+
 // Stop charging battery immediately
 void BQ25756::SafetyConfig::interruptCharging()
 {
@@ -82,4 +108,13 @@ void BQ25756::SafetyConfig::interruptCharging()
     uint8_t newValue = currValue & ~(0x01);
     writeRegister(CHARGER_CONT, newValue);
 }
+
+// Enable charging
+void BQ25756::SafetyConfig::enableCharging()
+{
+    uint8_t currValue = read8bitRegister(CHARGER_CONT);
+    uint8_t newValue = currValue | (0x01);
+    writeRegister(CHARGER_CONT, newValue);
+}
+
 
